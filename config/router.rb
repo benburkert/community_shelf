f6ad29bdc06b346849c080c8f7e7ac340fbc1082
Ben Burkert <ben@benburkert.com>
2008-10-03 19:06:07 -0500

    updated the router

--- config/router.rb
+++ config/router.rb
@@ -1,35 +1,41 @@
-# Merb::Router is the request routing mapper for the merb framework.
-#
-# You can route a specific URL to a controller / action pair:
-#
-#   r.match("/contact").
-#     to(:controller => "info", :action => "contact")
-#
-# You can define placeholder parts of the url with the :symbol notation. These
-# placeholders will be available in the params hash of your controllers. For example:
-#
-#   r.match("/books/:book_id/:action").
-#     to(:controller => "books")
-#   
-# Or, use placeholders in the "to" results for more complicated routing, e.g.:
-#
-#   r.match("/admin/:module/:controller/:action/:id").
-#     to(:controller => ":module/:controller")
-#
-# You can also use regular expressions, deferred routes, and many other options.
-# See merb/specs/merb/router.rb for a fairly complete usage sample.
-
 Merb.logger.info("Compiling routes...")
-Merb::Router.prepare do |r|
-  # RESTful routes
-  # r.resources :posts
+Merb::Router.prepare do
 
-  # This is the default route for /:controller/:action/:id
-  # This is fine for most cases.  If you're heavily using resource-based
-  # routes, you may want to comment/remove this line to prevent
-  # clients from calling your create or destroy actions with a GET
-  r.default_routes
-  
-  # Change this for your home page to be available at /
-  # r.match('/').to(:controller => 'whatever', :action =>'index')
+  identify(Review => :id) do
+    to(:controller => 'reviews') do
+      match('/review/:id').to(:action => 'show').name(:review)
+      match('/review/new', :method => 'post').to(:action => 'create').name(:new_review)
+    end
+  end
+
+  identify(Book => :slug) do
+    to(:controller => 'books') do
+      match('/book/:isbn', :isbn => /^((?:\d{10})|(?:\d{13}))$/).to(:action => 'isbn_lookup')
+
+      match('/books/:id/edit', :method => :get).to(:action => 'edit').name(:edit_book)
+
+      match('/books/new', :method => 'post').to(:action => 'create')
+      match('/books/new').to(:action => 'new').name(:new_book)
+
+      match('/books/:id/checkin', :method => :post).to(:action => 'checkin').name(:checkin)
+      match('/books/:id/checkout', :method => :post).to(:action => 'checkout').name(:checkout)
+
+      match('/book/:id').to(:action => 'show').name(:book)
+      match('/books/(:term)').to(:action => 'index').name(:books)
+    end
+  end
+
+  to(:controller => 'users') do
+    match('/signup', :method => :post).to(:action => 'create')
+    match('/signup').to(:action => 'new').name(:signup)
+  end
+
+  to(:controller => 'sessions') do
+    match('/login/complete').to(:action => 'create').name(:complete_login)
+    match('/login', :method => :post).to(:action => 'create')
+    match('/login').to(:action => 'new').name(:login)
+    match('/logout').to(:action => 'destroy').name(:logout)
+  end
+
+  match('/').to(:controller => 'dash', :action => 'index').name(:dash)
 end
